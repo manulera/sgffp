286f4394d68a2e0551d6a7055c8f97f45f56e31c
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-05-28 08:24:23 +0100

    new test examples for parsing

---
+++ quick_test2.py
@@ -0,0 +1,10 @@
+from sgffp.cli import _open_input
+import glob
+
+for file in glob.glob("degenerate_examples/*.dna"):
+
+    sgff = _open_input(file)
+    child_sequence = sgff.blocks[0][0]["sequence"]
+    parent_sequence = sgff.history.get_sequence_at(0)
+    print(parent_sequence)
+    print(child_sequence)
